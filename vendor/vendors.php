18e4aa90e92f42640f344b9c8d402e25dc539330
手嶋守 <tejima@gmail.com>
2012-02-15 02:41:32 +0900

    typo

---
+++ vendor/vendors.php
@@ -0,0 +1,26 @@
+#!/usr/bin/env php
+<?php
+
+set_time_limit(0);
+
+if (isset($argv[1])) {
+    $_SERVER['OPENPNE_BRANCH'] = $argv[1];
+}
+
+$vendorDir = __DIR__;
+$deps = array(
+    array('OpenPNE', 'http://github.com/houou/HOUOU', isset($_SERVER['OPENPNE_BRANCH']) ? $_SERVER['OPENPNE_BRANCH'] : 'tjm/apitest'),
+);
+
+foreach ($deps as $dep) {
+    list($name, $url, $rev) = $dep;
+
+    echo "> Installing/Updating $name\n";
+
+    $installDir = $vendorDir.'/'.$name;
+    if (!is_dir($installDir)) {
+        system(sprintf('git clone -q %s %s', escapeshellarg($url), escapeshellarg($installDir)));
+    }
+
+    system(sprintf('cd %s && git fetch -q origin && git reset --hard %s', escapeshellarg($installDir), escapeshellarg($rev)));
+}
